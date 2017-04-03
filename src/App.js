1e48eb1432f514c508b6c127938b64257c199efe
triyanarief <triyanarief@icloud.com>
2017-04-03 19:50:45 +0700

    set output

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Output from './Components/Output';
+import Select from './Components/Controls/Select';
+import Text from './Components/Controls/Text';
 import axios from 'axios';
 
 class App extends Component {
@@ -29,10 +31,30 @@ class App extends Component {
       });
   }
 
+  showHtml(x){
+      this.setState({html: x}, this.getSampleText);
+  }
+
+  changeParas(number){
+      this.setState({paras: number}, this.getSampleText);
+  }
 
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+        <h1 className="text-center">ReactJS Sample Text Generator</h1>
+        <hr />
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label>Include HTML:</label>
+            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+          </div>
+        </form>
+        <br /><br />
         <Output value={this.state.text} />
       </div>
     );
