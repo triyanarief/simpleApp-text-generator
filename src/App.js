ee7c8ee3a4a1d33e15d2ed56c7696538104906f5
triyanarief <triyanarief@icloud.com>
2017-04-03 19:47:13 +0700

    set output

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Output from './Components/Output';
 import axios from 'axios';
 
 class App extends Component {
@@ -32,7 +33,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello
+        <Output value={this.state.text} />
       </div>
     );
   }
